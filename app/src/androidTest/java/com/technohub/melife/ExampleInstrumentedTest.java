094079a797cbaeb12fff4055c06bae08a6f0d746
sajnasreejith <58685564+sajnasreejith@users.noreply.github.com>
2019-12-09 02:59:18 -0800

    initial commit

--- app/src/androidTest/java/com/technohub/melife/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/technohub/melife/ExampleInstrumentedTest.java
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.technohub.melife", appContext.getPackageName());
+        assertEquals("com.technohub.myapplication", appContext.getPackageName());
     }
 }
